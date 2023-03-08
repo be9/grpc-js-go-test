a090333723e3627493a0aeec4771d3dab43fa506
Oleg Dashevskii <odashevskii@plaid.com>
2023-03-08 11:37:45 +0700

    Add debug/requests

--- server/main.go
+++ server/main.go
@@ -5,15 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"github.com/be9/grpc-js-go-test/server/util"
+	"golang.org/x/net/trace"
 	"log"
 	"net"
+	"net/http"
 
 	pb "github.com/be9/grpc-js-go-test/proto/testservice"
 	"google.golang.org/grpc"
 )
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port     = flag.Int("port", 50051, "The server port")
+	httpPort = flag.Int("http-port", 50052, "The HTTP port")
 )
 
 type server struct {
@@ -54,13 +57,25 @@ func (s *server) GetFoobars(ctx context.Context, req *pb.GetFoobarsRequest) (*pb
 
 func main() {
 	flag.Parse()
+
+	trace.AuthRequest = func(req *http.Request) (bool, bool) {
+		return true, true
+	}
+	grpc.EnableTracing = true
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterTestServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
+	log.Printf("gRPC server listening at %v", lis.Addr())
+
+	go (func() {
+		log.Printf("Starting HTTP at :%d", *httpPort)
+		_ = http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), nil)
+	})()
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
